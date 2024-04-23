debf28dee6728759fcd0cbcf9ec3d496d0b1ea5d
Tanmay Pandey <tanmayiitroorkee17@gmail.com>
2024-04-23 17:16:55 +0530

    created login page

---
+++ src/api/authentication.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const BASE_URL = "https://ecommerce-server-black.vercel.app";
+
+export const login = async (user: any) => {
+    const res = await axios.post(`${BASE_URL}/auth/login`, user);
+    localStorage.setItem("token", res.data.accessToken);
+    return res.data;
+};
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,42 @@
+import DensityMediumIcon from '@mui/icons-material/DensityMedium';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import { useState } from 'react';
+
+const Navbar = () => {
+    const [open, setOpen] = useState(false);
+
+    const handleHamberger = () => {
+        console.log("first")
+        setOpen(open => !open);
+    }
+
+    return (
+        <div className="bg-[rgb(9,132,253)] flex items-center justify-between  py-[10px] px-[20px]">
+            <div className="flex items-center text-[20px] font-[700] text-[#fff] cursor-pointer">
+                <img src="https://nom.mathmaterate.com/static/media/Logo-lg.b059f4b405cf0d67d81c.png"
+                    alt="logo"
+                    className="!w-[50px] !h-[50px] mr-[10%]"
+                />
+                <span className="text-[rgb(255,214,0)]">M</span>ateRate
+            </div>
+            <div className="flex gap-[17px] text-[17px] text-[#fff] font-[500] max-sm:hidden">
+                <span className="cursor-pointer hover:text-yellow-400">Home</span>
+                <span className="cursor-pointer hover:text-yellow-400">About Us</span>
+                <span className="cursor-pointer hover:text-yellow-400">Login</span>
+            </div>
+            <DensityMediumIcon className="!hidden !text-[#fff] !cursor-pointer max-sm:!block" onClick={handleHamberger} />
+            {open &&
+                <div className='bg-[rgb(9,132,253)] w-full fixed top-0 left-0 right-0 z-[100]'>
+                    <CloseRoundedIcon className='!text-yellow-400 !cursor-pointer fixed top-1 right-1' onClick={handleHamberger} />
+                    <div className=" flex items-center flex-col gap-[17px] text-[17px] py-[5vh] text-[#fff] font-[500]">
+                        <span className="cursor-pointer hover:text-yellow-400">Home</span>
+                        <span className="cursor-pointer hover:text-yellow-400">About Us</span>
+                        <span className="cursor-pointer hover:text-yellow-400">Login</span>
+                    </div>
+                </div>
+            }
+        </div>
+    )
+}
+
+export default Navbar
